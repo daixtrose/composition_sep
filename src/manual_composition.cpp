171b60c1e036c63226a5fce0d67818a0542a1373
Markus Werle <daixtrose@users.noreply.github.com>
2020-01-28 11:25:39 +0100

    add RxCpp support as example

--- src/manual_composition.cpp
+++ src/manual_composition.cpp
@@ -16,28 +16,31 @@
 
 #include "client_component/client_component.hpp"
 #include "listener_component/listener_component.hpp"
-#include "talker_component/talker_component.hpp"
-#include "server_component/server_component.hpp"
 #include "rclcpp/rclcpp.hpp"
+#include "server_component/server_component.hpp"
+#include "talker_component/talker_component.hpp"
 
-int main(int argc, char *argv[])
-{
+#include "rxcpp/rx.hpp"
+
+int main(int argc, char *argv[]) {
   // Force flush of the stdout buffer.
   setvbuf(stdout, NULL, _IONBF, BUFSIZ);
 
-  // Initialize any global resources needed by the middleware and the client library.
-  // This will also parse command line arguments one day (as of Beta 1 they are not used).
-  // You must call this before using any other part of the ROS system.
-  // This should be called once per process.
+  // Initialize any global resources needed by the middleware and the client
+  // library. This will also parse command line arguments one day (as of Beta 1
+  // they are not used). You must call this before using any other part of the
+  // ROS system. This should be called once per process.
   rclcpp::init(argc, argv);
 
-  // Create an executor that will be responsible for execution of callbacks for a set of nodes.
-  // With this version, all callbacks will be called from within this thread (the main one).
+  // Create an executor that will be responsible for execution of callbacks for
+  // a set of nodes. With this version, all callbacks will be called from within
+  // this thread (the main one).
   rclcpp::executors::MultiThreadedExecutor exec;
   rclcpp::NodeOptions options;
 
-  // Add some nodes to the executor which provide work for the executor during its "spin" function.
-  // An example of available work is executing a subscription callback, or a timer callback.
+  // Add some nodes to the executor which provide work for the executor during
+  // its "spin" function. An example of available work is executing a
+  // subscription callback, or a timer callback.
   auto talker = std::make_shared<composition::Talker>(options);
   exec.add_node(talker);
   auto listener = std::make_shared<composition::Listener>(options);
@@ -47,8 +50,8 @@ int main(int argc, char *argv[])
   auto client = std::make_shared<composition::Client>(options);
   exec.add_node(client);
 
-  // spin will block until work comes in, execute work as it becomes available, and keep blocking.
-  // It will only be interrupted by Ctrl-C.
+  // spin will block until work comes in, execute work as it becomes available,
+  // and keep blocking. It will only be interrupted by Ctrl-C.
   exec.spin();
 
   rclcpp::shutdown();
